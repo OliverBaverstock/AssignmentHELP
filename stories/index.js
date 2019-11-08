8024fcee9ce55f1ae657eed4667c88ed789dd064
OliverBaverstock <oliverbaverstock@yahoo.ie>
2019-11-08 23:12:41 +0000

    Updated storybook for delete

--- stories/index.js
+++ stories/index.js
@@ -8,11 +8,11 @@ import DealList from '../src/components/dealList/'
 import DealsForm from '../src/components/dealsForm/';
 import FilterControls from '../src/components/filterControls/'
 
-
 const sample = {
   dishName: 'Spice Bag',
   restName: 'Imperial Garden',
   price: '9.99',
+  phone: '021-4762839',
   picture: './spicebag.jpg',
   upvotes: 10
 }
@@ -25,13 +25,13 @@ storiesOf("Deals List App/Filter Controls", module).add("default", () => (
   <FilterControls />
 )); 
 
-storiesOf("Deals List App/Deal", module).add("default", () => ( 
-  <Deal deal={sample}/>
-)); 
-
 storiesOf("Deals List App/Deal List", module).add("default", () => { 
   const samples = [sample, sample, sample, sample, sample]
   return <DealList deals={samples}/>
 });
 
 storiesOf("Deals List App/Deals Form", module).add("default", () => <DealsForm />);
+
+storiesOf("Deals List App/Deal", module).add("default", () => ( 
+  <Deal deal={sample} deleteHandler={action('Delete confirmed') }/>
+));
