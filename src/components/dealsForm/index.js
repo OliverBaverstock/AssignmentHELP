5ad668dbd74479d7842d9a81edb9f1cf775d579b
OliverBaverstock <oliverbaverstock@yahoo.ie>
2019-12-02 10:28:40 +0000

    Added route for add form

--- src/components/dealsForm/index.js
+++ src/components/dealsForm/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './dealsForm.css';
+import api from '../../dataStore/stubAPI';
 
 export default class Form extends Component {
     state = { dishName: '', restName: '', price: '',phone: '', picture: ''};
@@ -12,7 +13,7 @@ export default class Form extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        this.props.handleAdd( this.state.dishName, this.state.restName, this.state.price, this.state.phone, this.state.picture)
+        api.add( this.state.dishName, this.state.restName, this.state.price, this.state.phone, this.state.picture)
         this.setState({ dishName: '', restName:'', price: '', phone:'', picture: ''})
     }
 
